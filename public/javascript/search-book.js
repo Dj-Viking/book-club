19f0c6ac0224ce3249ac23d7bf0f4f288e6754af
DJ Viking <anders.swedishviking@gmail.com>
2020-09-12 16:04:00 -0400

    moved book search results to a partial and general refactor

--- public/javascript/search-book.js
+++ public/javascript/search-book.js
@@ -2,7 +2,7 @@ const postErrEl = document.querySelector('#search-err');
 const searchFormHandler = async (event) => {
   try {
     event.preventDefault();
-
+    //using HTML get form in the book-search.handlebars!
     const book_title = document.querySelector('input[name="book_title"]').value.trim();
     const author = document.querySelector('input[name="author"]').value.trim();
     console.log(book_title, author);
@@ -15,25 +15,6 @@ const searchFormHandler = async (event) => {
       }, 3000);
       throw new Error("Search field can't be empty.")
     }
-    // const response = await fetch(`/book-search/q?=${book_title}&${author}`, {
-    //   method: 'GET'
-    //   // body: JSON.stringify(
-    //   //   {
-    //   //     book_title,
-    //   //     author
-    //   //   }
-    //   // ),
-    //   // headers: {
-    //   //   'Content-Type': 'application/json'
-    //   // }
-    // });
-    // if (response.ok) {
-    //   // const json = await response.json();
-    //   // document.location.reload();
-    //   console.log("got the response");
-    // }
-    // // console.log("There was an error.");
-    // console.log(response.statusText);
   } catch (error) {
     console.log(error);
   }
