5c283fe026d079198c091f257fe1ae118b9abd7f
carolina <carolinaccez@gmail.com>
2020-09-14 16:00:01 -0400

    logged in navbar

--- public/javascript/login-signup.js
+++ public/javascript/login-signup.js
@@ -42,7 +42,7 @@ const signupFormHandler = async (event) => {
     });
     if (response.ok) {
 
-      document.location.reload(); 
+      document.location.replace('/my-library');
     } else {
       console.log("There was an error."); 
       console.log(response.statusText);
